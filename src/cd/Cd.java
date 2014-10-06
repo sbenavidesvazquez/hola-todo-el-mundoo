8851e03d97469c6d3d05180ec1ae2f5302c7a504
sbenavidesvazquez <sbenavidesvazquez@eniac22.danielcastelao.org>
2014-10-06 09:20:46 +0200

    commit inicial

---
+++ src/cd/Cd.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cd;
+
+/**
+ *
+ * @author sbenavidesvazquez
+ */
+public class Cd {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Hola todo el mundooo, hola doctor niiiick");
+    }
+    
+}
